67f79233923fc49133ab87050542ec7c44c018c2
chad-js <chadflemies@gmail.com>
2024-01-10 19:50:02 -0500

    Emit OwnableTrigger deployed event with init metadata

--- src/OwnableTriggerFactory.sol
+++ src/OwnableTriggerFactory.sol
@@ -2,19 +2,40 @@
 pragma solidity 0.8.22;
 
 import {OwnableTrigger} from "./OwnableTrigger.sol";
+import {TriggerMetadata} from "./structs/Triggers.sol";
 
 contract OwnableTriggerFactory {
+  /// @dev Emitted when the factory deploys a trigger.
+  /// @param trigger The address at which the trigger was deployed.
+  /// @param owner The owner of the trigger.
+  /// @param name The human-readble name of the trigger.
+  /// @param category The category of the trigger.
+  /// @param description A human-readable description of the trigger.
+  /// @param logoURI The URI of a logo image to represent the trigger.
+  /// For other attributes, see the docs for the params of `deployTrigger` in
+  /// this contract.
+  event TriggerDeployed(
+    address trigger, address indexed owner, string name, string category, string description, string logoURI
+  );
+
   /// @notice Deploys a new OwnableTrigger contract with the supplied owner and deploy salt.
   /// @param _owner The owner of the trigger.
+  /// @param _metadata The metadata of the trigger.
   /// @param _salt Used during deployment to compute the address of the new OwnableTrigger.
-  function deployTrigger(address _owner, bytes32 _salt) external returns (OwnableTrigger) {
-    return new OwnableTrigger{salt: _salt}(_owner);
+  function deployTrigger(address _owner, TriggerMetadata memory _metadata, bytes32 _salt)
+    external
+    returns (OwnableTrigger _trigger)
+  {
+    _trigger = new OwnableTrigger{salt: _salt}(_owner);
+    emit TriggerDeployed(
+      address(_trigger), _owner, _metadata.name, _metadata.category, _metadata.description, _metadata.logoURI
+    );
   }
 
   /// @notice Call this function to determine the address at which a trigger
   /// with the supplied configuration would be deployed. See `deployTrigger` for
   /// more information on parameters and their meaning.
-  function computeTriggerAddress(address _owner, bytes32 _salt) public view returns (address _address) {
+  function computeTriggerAddress(address _owner, bytes32 _salt) external view returns (address _address) {
     // https://eips.ethereum.org/EIPS/eip-1014
     bytes32 _bytecodeHash = keccak256(bytes.concat(type(OwnableTrigger).creationCode, abi.encode(_owner)));
     bytes32 _data = keccak256(bytes.concat(bytes1(0xff), bytes20(address(this)), _salt, _bytecodeHash));
